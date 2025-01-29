a20dc3554e7adeaf8ba011f0ea4c2b4faee73f3c
Orkhan Rutherford <orkhanruth@gmail.com>
2025-01-29 18:19:21 +0400

    Update install-ligolo.sh

--- install/install-ligolo.sh
+++ install/install-ligolo.sh
@@ -14,10 +14,10 @@ sudo wget \
 #    https://github.com/nicocha30/ligolo-ng/releases/download/v0.7.5/ligolo-ng_agent_0.7.5_windows_amd64.zip \
 #    https://github.com/nicocha30/ligolo-ng/releases/download/v0.7.5/ligolo-ng_proxy_0.7.5_windows_amd64.zip
 
-sudo tar -xzf ligolo-ng_agent_0.7.5_linux_amd64.tar.gz
-sudo tar -xzf ligolo-ng_proxy_0.7.5_linux_amd64.tar.gz
-sudo unzip -o ligolo-ng_agent_0.7.5_windows_amd64.zip
-sudo unzip -o ligolo-ng_proxy_0.7.5_windows_amd64.zip
+sudo tar -xzf ligolo-ng_agent_0.5.2_linux_amd64.tar.gz
+sudo tar -xzf ligolo-ng_proxy_0.5.2_linux_amd64.tar.gz
+sudo unzip -o ligolo-ng_agent_0.5.2_windows_amd64.zip
+sudo unzip -o ligolo-ng_proxy_0.5.2_windows_amd64.zip
 
 sudo mv agent linux/agent
 sudo mv proxy linux/proxy
